75d843c42518a006560c236f264b043bbf7827e5
Mahabubul Hasan <codehasan@gmail.com>
2021-07-20 16:18:15 +0600

    Add project files.

---
+++ Client/Program.cs
@@ -0,0 +1,65 @@
+﻿using GrainInterfaces;
+using Microsoft.Extensions.Logging;
+using Orleans;
+using Orleans.Configuration;
+using System;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            return RunMainAsync().Result;
+        }
+
+        private static async Task<int> RunMainAsync()
+        {
+            try
+            {
+                using(var client  = await ConnectClientAsync())
+                {
+                    await DoClientWorkAsync(client);
+                    Console.ReadKey();
+                }
+
+                return 0;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"\nException while trying to run client: {e.Message}");
+                Console.WriteLine("Make sure the silo the client is trying to connect to is running.");
+                Console.WriteLine("\nPress any key to exit.");
+                Console.ReadKey();
+
+                return -1;
+            }
+        }
+
+        private static async Task<IClusterClient> ConnectClientAsync()
+        {
+            IClusterClient client;
+            client = new ClientBuilder()
+                .UseLocalhostClustering()
+                .Configure<ClusterOptions>(options =>
+                {
+                    options.ClusterId = "dev";
+                    options.ServiceId = "OrleansBasics";
+                })
+                .ConfigureLogging(logging => logging.AddConsole())
+                .Build();
+
+            await client.Connect();
+            Console.WriteLine("Client successfully connected to silo host \n");
+            return client;
+        }
+
+        private static async Task DoClientWorkAsync(IClusterClient client)
+        {
+            var friend = client.GetGrain<IHello>(0);
+            var response = await friend.SayHello("Good morning, HelloGrain!");
+            Console.WriteLine($"\n\n {response} \n\n");
+        }
+    }
+}
---
+++ Grains/HelloGrain.cs
@@ -0,0 +1,22 @@
+﻿using GrainInterfaces;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+
+namespace Grains
+{
+    public class HelloGrain: Orleans.Grain, IHello
+    {
+        private readonly ILogger<HelloGrain> logger;
+
+        public HelloGrain(ILogger<HelloGrain> logger)
+        {
+            this.logger = logger;
+        }
+
+        public Task<string> SayHello(string greetings)
+        {
+            logger.LogInformation($"\n SayHello message received: greeting = '{greetings}'");
+            return Task.FromResult($"\n Client said: '{greetings}', so HelloGrain says: Hello!");
+        }
+    }
+}
